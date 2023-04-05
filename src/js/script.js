2592979d2bbb2135bc6b20fb599f81c9b9fa137e
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-04-05 15:30:44 +0200

    Add function that adding books to favorite by doubleclick

--- src/js/script.js
+++ src/js/script.js
@@ -11,22 +11,67 @@
     books: Handlebars.compile(document.querySelector(select.bookTemplate).innerHTML),
   };
 
-  function renderBooks(dataSource){
-    const booksList = document.querySelector(select.booksList);
+  class BooksList {
+    constructor(){
+      const thisBookList = this;
 
-    dataSource.books.forEach(bookData => {
+      thisBookList.getElements();
+      thisBookList.render(dataSource);
+      thisBookList.favouritefilter();
+    }
 
-      // Generate HTML based on HandleBars Template
-      const generatedHTML = templates.books(bookData);
+    getElements(){
+      const thisBookList = this;
 
-      // Create bookElement using utils.createElementFromHTML
-      const bookElement = utils.createDOMFromHTML(generatedHTML);
+      thisBookList.booksContainer = document.querySelector(select.booksList);
 
-      // Adding element to list .books-list
-      booksList.appendChild(bookElement);
+    }
+
+    render(dataSource){
+    /* FUNCTION TO RENDER BOOKS AT PAGE */
+      const thisBookList = this;
+
+      dataSource.books.forEach(bookData => {
+
+        // Generate HTML based on HandleBars Template
+        const generatedHTML = templates.books(bookData);
+
+        // Create bookElement using utils.createElementFromHTML
+        const bookElement = utils.createDOMFromHTML(generatedHTML);
+
+        // Adding element to list .books-list
+        thisBookList.booksContainer.appendChild(bookElement);
+
+      });
+    }
+    /* Function to add books to favourite */
+    favouritefilter(){
+      const thisBookList = this;
+      const favoriteBooks = [];
+
+      thisBookList.booksContainer.addEventListener('dblclick', function(event){
+        event.preventDefault();
+
+        // return parent element of the element that triggered the event.target and get it data-id attribute
+        const bookId = event.target.offsetParent.getAttribute('data-id');
 
 
-    });
+        if (!favoriteBooks.includes(bookId)){
+          // add parent element class favorite
+          event.target.offsetParent.classList.add('favorite');
+          favoriteBooks.push(bookId);
+        } else {
+          // remove parent element class favorite
+          event.target.offsetParent.classList.remove('favorite');
+          favoriteBooks.pop(bookId);
+        }
+
+        console.log(favoriteBooks);
+      });
+    }
+
   }
-  renderBooks(dataSource);
+
+  const app = new BooksList();
+  console.log(app);
 }
